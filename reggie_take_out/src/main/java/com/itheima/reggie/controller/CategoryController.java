0694d05108e2165c246c48217709676fa63a229f
Jason Ping <75838453+PING-An32@users.noreply.github.com>
2024-06-06 20:52:41 +0800

    6_6 commit

--- reggie_take_out/src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -9,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/category")
 @Slf4j
@@ -43,6 +45,44 @@ public class CategoryController {
         categoryService.page(pageInfo, queryWrapper);
 
         return R.success(pageInfo);
+    }
+
+    //Requested URL: localhost:8080/category?id=xxxxxx
+
+    /**
+     * 根据id删除分类
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    public R<String> delete(Long ids){//不需要加RequestBody Pathvariable
+        log.info("删除分类，id为：{}",ids);
+
+//        categoryService.removeById(id);
+        categoryService.remove(ids);
+        return R.success("分类信息删除成功");
+    }
+
+    @PutMapping
+    public R<String> update(@RequestBody Category category){//json格式需要加requestbody注解
+        log.info("修改分类信息：{}",category);
+        categoryService.updateById(category);
+        return R.success("修改分类信息成功");
+    }
+
+    /**
+     * 根据条件查询分类数据
+     * @param category
+     * @return
+     */
+    @GetMapping("/list")
+    public R<List<Category>> list(Category category){
+        LambdaQueryWrapper<Category> queryWrapper =new LambdaQueryWrapper<>();
+        queryWrapper.eq(category.getType()!=null,Category::getType,category.getType());
+        queryWrapper.orderByAsc(Category::getSort).orderByDesc(Category::getUpdateTime);
+
+        List<Category> list = categoryService.list(queryWrapper);
+        return R.success(list);
     }
 
 }
---
+++ reggie_take_out/src/test/java/leetcode.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class leetcode {
+
+
+}
